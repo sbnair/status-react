14e4710253ff9d7efbb2a1d409fbc78c26795a73
Vitaliy Vlasov <siphiuel@gmail.com>
2020-05-05 12:47:18 +0300

    Add bogus darwin target

--- nix/shells.nix
+++ nix/shells.nix
@@ -73,6 +73,7 @@ let
     android = targets.mobile.android.shell;
     desktop = targets.desktop.shell;
     linux = targets.desktop.linux.shell;
+    darwin = targets.desktop.macos.shell;
     macos = targets.desktop.macos.shell;
     windows = targets.desktop.macos.shell;
   };
