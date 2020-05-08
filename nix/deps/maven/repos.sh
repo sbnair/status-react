730164f23c6a42b3f4fef3aff4c844dd64e057c6
Jakub Sokołowski <jakub@status.im>
2020-05-08 14:52:16 +0200

    w
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/deps/maven/repos.sh
+++ nix/deps/maven/repos.sh
@@ -4,13 +4,11 @@
 
 declare -A REPOS=(
   [apache]="https://repo.maven.apache.org/maven2"
-  [clojars]="https://repo.clojars.org"
-  [fabric-io]="https://maven.fabric.io/public"
+  [gradle]="https://plugins.gradle.org/m2"
   [google]="https://dl.google.com/dl/android/maven2"
+  [sonatype]="https://repository.sonatype.org/content/groups/sonatype-public-grid"
   [java]="https://maven.java.net/content/repositories/releases"
   [jcenter]="https://jcenter.bintray.com"
   [jitpack]="https://jitpack.io"
   [maven]="https://repo1.maven.org/maven2"
-  [gradle]="http://repo.gradle.org/gradle/libs-releases-local"
-  [gradlePlugins]="https://plugins.gradle.org/m2"
 )
